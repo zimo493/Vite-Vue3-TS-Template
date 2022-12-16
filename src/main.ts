41936bb82143f224e7424964b52db0f1287034fd
hyf <123456>
2022-12-16 14:45:44 +0800

    update🌞

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@
  */
 import { createApp } from 'vue'
 import './style.css' /* 开发中复制src/assets/styles/base.css替换掉./style.css默认文件中的内容 并删除base.css文件 可在src/assets/styles/comm.less中编写项目中公共样式 */
-
+// import '@/utils/permissions'; // 路由权限校验
 import 'element-plus/dist/index.css'; // element plus样式
 // import 'element-plus/theme-chalk/dark/css-vars.css'; // 暗黑模式主题
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'; // 导入所有element icon图标
