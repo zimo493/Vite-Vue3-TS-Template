afa697f09bf00e77aa6dc62eff4a2335fbd7cd42
胡子陌 <88411811+zimo493@users.noreply.github.com>
2022-12-30 09:44:23 +0800

    Update login.ts

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,7 +2,7 @@ import requests from "@/utils/request";
 import { AxiosPromise } from "axios";
 
 /* 登录示例 */
-export function login(data: { username: string; password: string; }): AxiosPromise {
+export const login = (data: { username: string; password: string; }): AxiosPromise => {
   return requests({
     url: "/user/login",
     method: "post",
